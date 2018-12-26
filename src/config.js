ce651537d5d9c77cefb860b6a6ea1466d6be154a
Gabriel <gabriel_luchtenberg@edu.univali.br>
2018-12-26 14:30:39 -0200

    agora vai

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ const dev = {
 const prod = {
     BACKEND_API: 'http://54.233.192.91:8000/api/v1'
 }
-console.log(process.env.NODE_ENV)
+
 const config = process.env.NODE_ENV === 'development'  ?
                     dev :
                     prod
